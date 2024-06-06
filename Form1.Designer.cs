cede51ddf2e10e719c94f37c66135d289d72e080
spiro <spiro@LAPTOP-SBM34TKV>
2024-06-06 18:33:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace All_Caps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.conv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.input.Location = new System.Drawing.Point(28, 24);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(714, 142);
+            this.input.TabIndex = 0;
+            // 
+            // output
+            // 
+            this.output.CausesValidation = false;
+            this.output.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.output.Location = new System.Drawing.Point(28, 270);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(714, 142);
+            this.output.TabIndex = 2;
+            this.output.Text = "Your Text will be displayed here";
+            // 
+            // conv
+            // 
+            this.conv.Image = global::All_Caps.Properties.Resources.all_caps_logo_onlyletters;
+            this.conv.Location = new System.Drawing.Point(205, 172);
+            this.conv.Name = "conv";
+            this.conv.Size = new System.Drawing.Size(359, 92);
+            this.conv.TabIndex = 1;
+            this.conv.UseVisualStyleBackColor = true;
+            this.conv.Click += new System.EventHandler(this.conv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.conv);
+            this.Controls.Add(this.input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button conv;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace All_Caps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conv_Click(object sender, EventArgs e)
+        {
+            output.Text = input.Text.ToUpper();
+        }
+
+        private void output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
